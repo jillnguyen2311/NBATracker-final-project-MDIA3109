ae1b09c7f06f93ba67ca1d7dfc38b9864bbb98e2
JesseWelk <jwelk@my.bcit.ca>
2024-02-19 21:58:21 -0800

    Routed more pages, added home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
 import Image from 'next/image'
 import Nav from './components/Nav'
-import Card from './components/Card'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <body style={{ fontFamily: "Almarai, sans-serif" }}>
+      <Head>
+        <title>Home</title>
+      </Head>
       <main>
-        <Nav/>
-        <Card/>
+        <Nav />
+        <h1 style={{ alignContent: "center", textAlign: "center", marginTop: "400px", fontSize: "2.9em" }}>"Your One Stop For All The Odds"</h1>
       </main>
     </body>
   )
