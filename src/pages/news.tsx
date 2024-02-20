5babdbc01a5c0c08db123d9e100e2bafe13f3a75
jillnguyen2311 <nguyennuduyhoai75@gmail.com>
2024-02-20 02:28:17 -0800

    update news style

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -12,7 +12,7 @@ interface Article {
 export default function News() {
   const [news, setNews] = useState<Article[]>([]);
   const apiUrl = 'https://nba-latest-news.p.rapidapi.com/articles';
-  const rapidApiKey = '708227765cmshd45426b5899e082p15bca7jsn871aa32efb19'; 
+  const rapidApiKey = '708227765cmshd45426b5899e082p15bca7jsn871aa32efb19';
 
   useEffect(() => {
     async function fetchNews() {
@@ -26,7 +26,7 @@ export default function News() {
           }
         };
         const response = await axios.request(options);
-        console.log(response.data); // Log response data for debugging
+        console.log(response.data);
         setNews(response.data.map((item: any) => ({ title: item.title, url: item.url, source: item.source })));
       } catch (error) {
         console.error('Error fetching news:', error);
@@ -37,23 +37,25 @@ export default function News() {
   }, []);
 
   return (
-    <main style={{ fontFamily: 'Almarai, sans-serif' }}>
+    <main className="font-Almarai">
       <Head>
         <title>Latest News</title>
       </Head>
       <Nav />
       <h1 className="text-black text-center font-bold text-4xl py-10">Latest News</h1>
-      <div>
+      <div className="mx-24 mb-8">
         {news.length > 0 ? (
           news.map((article, index) => (
-            <div key={index}>
-              <h2>{article.title}</h2>
-              <p>Source: {article.source}</p>
-              <a href={article.url} target="_blank" rel="noopener noreferrer">Read more</a>
+            <div key={index} className="border-4 border-green-500 rounded-lg shadow-md p-7 mb-8">
+              <h2 className="text-2xl font-semibold">{article.title}</h2>
+              <p className="text-gray-600 mt-3">Source: {article.source}</p>
+              <div className="mt-8">
+                <a href={article.url} className="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">Read more ➜</a> {/* Added mt-4 for top margin */}
+              </div>
             </div>
           ))
         ) : (
-          <p>No news available</p>
+          <p className="text-center">No news available</p>
         )}
       </div>
     </main>
