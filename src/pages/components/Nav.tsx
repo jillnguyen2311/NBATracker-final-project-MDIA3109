8d680d0608a57d44900df7a750b9824a24e0a96b
JesseWelk <jwelk@my.bcit.ca>
2024-02-19 21:10:07 -0800

    responsive nav bar

--- src/pages/components/Nav.tsx
+++ src/pages/components/Nav.tsx
@@ -1,16 +1,59 @@
 import styles from '../../styles/Nav.module.css'
 import Image from 'next/image'
+import { useState, useEffect } from 'react'
 
 export default function Nav() {
+
+    const [isMenuActive, setIsMenuActive] = useState(false);
+
+    useEffect(() => {
+        const hamburger = document.querySelector(`.${styles.hamburger}`);
+        const navMenu = document.querySelector(`.${styles.navmenu}`);
+        const navLink = document.querySelectorAll(`.${styles.navlink}`);
+
+        const mobileMenu = () => {
+            setIsMenuActive(!isMenuActive);
+        };
+
+        const closeMenu = () => {
+            setIsMenuActive(false);
+        };
+
+        hamburger!.addEventListener("click", mobileMenu);
+        navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+        return () => {
+            hamburger!.removeEventListener("click", mobileMenu);
+            navLink.forEach((n) => n.removeEventListener("click", closeMenu));
+        };
+    }, [isMenuActive]);
+
     return (
         <nav className={styles.nav}>
-            <div className={styles.NavContent}>
-                <Image src="/images/OddBall.png" alt="logo" width={150} height={150} />
-                <a href="#">Player Odds</a>
-                <a href="#">Live Games</a>
-                <a href="#">Latest News</a>
-                <a href="#">Player Stats</a>
-                <a href="#">Season Stats</a>
+            <div className={styles.navbar}>
+                <Image className={styles.space} src="/images/OddBall.png" alt="logo" width={150} height={150} />
+                <ul className={`${styles.navmenu} ${isMenuActive ? styles.active : ''}`}>
+                    <li className={styles.navitem}>
+                        <a href="#" className={styles.navlink}>Player Odds</a>
+                    </li>
+                    <li className={styles.navitem}>
+                        <a href="#" className={styles.navlink}>Live Games</a>
+                    </li>
+                    <li className={styles.navitem}>
+                        <a href="#" className={styles.navlink}>Latest News</a>
+                    </li>
+                    <li className={styles.navitem}>
+                        <a href="#" className={styles.navlink}>Player Stats</a>
+                    </li>
+                    <li className={styles.navitem}>
+                        <a href="#" className={styles.navlink}>Season Stats</a>
+                    </li>
+                </ul>
+                <div className={`${styles.hamburger} ${isMenuActive ? styles.active : ''}`}>
+                    <span className={styles.bar}></span>
+                    <span className={styles.bar}></span>
+                    <span className={styles.bar}></span>
+                </div>
             </div>
         </nav>
     )
