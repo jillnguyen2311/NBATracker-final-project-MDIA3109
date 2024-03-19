49c4ac2929941d68b0fab4555297ce9ed05bf30f
JesseWelk <jwelk@my.bcit.ca>
2024-03-18 20:35:20 -0700

    made footer and about page

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -1,17 +1,58 @@
-import styles from "../../styles/Footer.module.css"
-
 export default function Footer() {
- return(
+    return (
 
-<div className={styles.footer}>
-    <div>
-        
-    </div>
-    <div>
-        OddBall 2024 ©
-    </div>
-</div>
-
- )
+        <footer className="text-[#595959] body-font">
+            <div className="container px-5 py-24 mx-auto">
+                <div className="flex flex-wrap md:text-left text-center order-first">
+                    <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+                        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+                        <nav className="list-none mb-10">
+                            <li>
+                                <a href="/live-games" className="text-[#595959] hover:text-gray-900 hover:underline">Live Games</a>
+                            </li>
+                            <li>
+                                <a href="/news" className="text-[#595959] hover:text-gray-900 hover:underline">Latest News</a>
+                            </li>
+                            <li>
+                                <a href="/season-stats" className="text-[#595959] hover:text-gray-900 hover:underline">Season Stats</a>
+                            </li>
+                            <li>
+                                <a href="/about" className="text-[#595959] hover:text-gray-900 hover:underline">About</a>
+                            </li>
+                        </nav>
+                    </div>
+                    <div className="lg:w-1/4 md:w-1/2 w-full px-4 ">
+                        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">SUBSCRIBE</h2>
+                        <div className="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start">
+                            <div className="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
+                                <label htmlFor="footer-field" className="leading-7 text-sm text-[#595959]">Join our mailing list</label>
+                                <input type="text" id="footer-field" name="footer-field" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-yellow-200 focus:border-yellow-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></input>
+                            </div>
+                            <a href="/sign-up" className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-[#FC9F5B] border-0 py-2 px-6 focus:outline-none hover:bg-[#FC9F5B] rounded">
+                                <button className="mb-0.5">Sign Up</button>
+                            </a>
+                        </div>
+                        <p className="text-gray-500 text-sm mt-2 md:text-left text-center">Don't miss a thing
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-gray-200">
+                <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
+                    <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+                        <img src="/images/OddBall.png" className="mt-2"></img>
+                    </a>
+                    <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2024 OddBall</p>
+                    <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+                        <a href="https://github.com/jillnguyen2311/NBATracker-final-project-MDIA3109" className="ml-3 text-gray-500">
+                            <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" className="w-8 h-8" viewBox="0 0 24 24">
+                                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                            </svg>
+                        </a>
+                    </span>
+                </div>
+            </div>
+        </footer>
+    )
 }
 
