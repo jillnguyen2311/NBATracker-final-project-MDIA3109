9b6c818a3d29e8ce5b3f9942f39cd7571d224e6a
Farbod Tandas <72369418+farbodtandas@users.noreply.github.com>
2024-03-22 15:50:26 -0700

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -38,7 +38,7 @@ export default function PlayerOdds() {
                         </div>
                         <div className="p-4 lg:w-1/2">
                             <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-                                <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="/images/farbod.jpeg"></img>
+                                <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="/images/Farbod.jpeg"></img>
                                 <div className="flex-grow sm:pl-8">
                                     <h2 className="title-font font-medium text-lg text-gray-900">Farbod Tandas</h2>
                                     <h3 className="text-gray-500 mb-3">UI Developer/Designer</h3>
