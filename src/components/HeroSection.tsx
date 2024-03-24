42fe65826151e05872499dd7f1df9310a4e2898b
jillnguyen2311 <nguyennuduyhoai75@gmail.com>
2024-03-24 05:50:10 -0700

    link landing page to home page

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,4 +1,5 @@
 import Spline from '@splinetool/react-spline';
+import Link from 'next/link';
 
 export default function HeroSection() {
   return (
@@ -7,7 +8,9 @@ export default function HeroSection() {
         <h1 className="text-5xl font-bold mb-4 px-4 sm:px-6 md:px-10 text-center">Welcome to OddBall</h1>
         <p className="text-lg mb-6">Your destination for sport game odds insights</p>
         <p className="text-md mx-6 px-5 md:mx-8 text-center md:text-center ">A live NBA sports tracking website that tracks the game scores and other details while simultaneously tracking the game's betting odds during live games. Search for your team, find their live scores, stats, and when and where they are playing.</p>
-        <button className="px-8 py-3 mt-10 bg-emerald-500 text-white font-semibold rounded-md hover:bg-orange-400 items-center justify-center">Get Started</button>
+        <Link href='/home'>
+          <button className="px-8 py-3 mt-10 bg-emerald-500 text-white font-semibold rounded-md hover:bg-orange-400 items-center justify-center">Get Started</button>
+        </Link>
       </div>
       <div className="md:w-1/2 flex items-center justify-center">
         <Spline scene="https://prod.spline.design/Jfl07f7b7WWOsD-6/scene.splinecode" />
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,63 +1,72 @@
-import Head from 'next/head';
-import HeroSection from '../components/HeroSection';
-import PricingCard from '../components/PricingCard';
-import SubBanner from '../components/SubBanner';
-import NewsletterSection from '../components/NewsletterSection';
-import Nav from '../components/Nav'
-import Footer from '../components/Footer';
-import styles from '../styles/index.module.css'
+// Home.tsx
 
-export default function LandingPage() {
+import Nav from '../components/Nav'
+import Head from 'next/head'
+import Footer from '../components/Footer'
+import styles from '../styles/index.module.css'
+import Card from '../components/Card'
+
+export default function Home() {
   return (
-    <main>
+    <main style={{ fontFamily: "Almarai, sans-serif" }}>
       <Head>
-        <title>OddBall Landing Page</title>
+        <title>Home</title>
       </Head>
       <div className={styles.header}>
         <Nav />
       </div>
 
-      <HeroSection />
-
-      <div className="container mx-auto px-4 py-8 flex flex-wrap justify-center">
-        <PricingCard
-          type="Basic"
-          price="$9.99/month"
-          features={[
-            "> Access to live game odds for major sports leagues.",
-            "> Real-time updates on team statistics and player performance.",
-            "> Limited access to premium features and insights.",
-            "> Monthly newsletter with industry news and betting tips."
-          ]}
-          splineScene="https://prod.spline.design/IHc1uMe7VBOrf4wZ/scene.splinecode"
-        />
-        <PricingCard
-          type="Standard"
-          price="$19.99/month"
-          features={[
-            "> Full access to live game odds for all sports leagues worldwide.",
-            "> Advanced analytics tools for in-depth game analysis.",
-            "> Exclusive insights into betting trends and market predictions.",
-            "> 24/7 customer support for any inquiries or assistance."
-          ]}
-          splineScene="https://prod.spline.design/K7-Jq7TsvvGdHNJk/scene.splinecode"
-        />
-        <PricingCard
-          type="Premium"
-          price="$29.99/month"
-          features={[
-            "> VIP access to expert betting strategies and tips.",
-            "> Customizable alerts for game events and betting opportunities.",
-            "> Personalized dashboard with tailored recommendations.",
-            "> Priority customer support with dedicated account manager."
-          ]}
-          splineScene="https://prod.spline.design/9YgA-in5C8QtAi-5/scene.splinecode"
-        />
+      <div className={styles.hero}>
+        <div className={styles.one}>
+          <div className={styles.contentContainer}>
+            <h1 style={{ fontSize: "60px", textAlign: "left", color: "#FBFFF4", fontWeight: "bold", transform: `skew(30deg)` }}>
+              This Is Basketball
+            </h1>
+            <p style={{ fontSize: "20px", textAlign: "left", color: "#FBFFF4", transform: `skew(30deg)`, maxWidth: "500px", paddingLeft: "70px" }}>
+              Find the most accurate and latest information regarding basketball here at OddBall.
+              From live game odds to player and team stats, everything you will ever need to find will be here.
+            </p>
+          </div>
+        </div>
+        <div className={styles.two}>
+          <img src="/images/nikola.png" alt="Player Nikola" className={`${styles.playerImage} ${styles.animation}`} />
+        </div>
       </div>
 
-      <SubBanner />
-      <NewsletterSection />
-      <Footer />
+      <div className={styles.live}>
+        <div className={styles.liveText}>
+          <h1 style={{ fontSize: "50px", textAlign: "center", color: "#595959", fontWeight: "bold", marginBottom: "40px" }}>Live Games</h1>
+        </div>
+        <div className={styles.cardContainer}>
+          <Card />
+          <Card />
+        </div>
+      </div>
+
+      <div className={styles.partner}>
+        <h1 style={{ fontSize: "50px", textAlign: "center", color: "#FBFFF4", fontWeight: "bold", marginBottom: "20px" }}>Partners</h1>
+        <div className={styles.partnerName}>
+          <h2>Edge</h2>
+          <h2>NBA</h2>
+          <h2>T-Mobile</h2>
+          <h2>Rogers</h2>
+          <h2>ESPN</h2>
+        </div>
+      </div>
+
+      <div className={styles.live}>
+        <div className={styles.liveText}>
+          <h1 style={{ fontSize: "50px", textAlign: "center", color: "#595959", fontWeight: "bold", marginBottom: "40px" }}>Current News</h1>
+        </div>
+        <div className={styles.cardContainer}>
+          <Card />
+          <Card />
+        </div>
+      </div>
+
+      <div className={styles.footer}>
+        <Footer />
+      </div>
     </main>
-  );
+  )
 }
