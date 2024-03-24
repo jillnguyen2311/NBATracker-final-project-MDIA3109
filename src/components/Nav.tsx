2a0cc18d3a544a781b6860af206011a061151c41
JesseWelk <jwelk@my.bcit.ca>
2024-03-24 15:01:03 -0700

    Navbar redo

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -31,30 +31,41 @@ export default function Nav() {
     return (
         <nav className={styles.nav}>
             <div className={styles.navbar}>
-                <a href="/home">
-                <Image className={styles.space} src="/images/OddBall.png" alt="logo" width={150} height={150} />
-                </a>
-                <ul className={`${styles.navmenu} ${isMenuActive ? styles.active : ''}`}>
-                    <li className={styles.navitem}>
-                        <a href="/live-games" className={styles.navlink}>Live Games</a>
-                    </li>
-                    <li className={styles.navitem}>
-                        <a href="/news" className={styles.navlink}>Latest News</a>
-                    </li>
-                    <li className={styles.navitem}>
-                        <a href="/season-stats" className={styles.navlink}>Season Stats</a>
-                    </li>
-                    <li className={styles.navitem}>
-                        <a href="/about" className={styles.navlink}>About</a>
-                    </li>
-                    <li className={styles.signup}>
-                        <a href="/sign-up" className={styles.navlink}>Sign Up</a>
-                    </li>
-                </ul>
-                <div className={`${styles.hamburger} ${isMenuActive ? styles.active : ''}`}>
-                    <span className={styles.bar}></span>
-                    <span className={styles.bar}></span>
-                    <span className={styles.bar}></span>
+                <div className='bg-[#FBFFF4] flex flex-row w-full p-5'>
+                    <a href="/home">
+                        <Image className={styles.space} src="/images/OddBall.png" alt="logo" width={150} height={150} />
+                    </a>
+                    <div className='absolute right-5 mb-5'>
+                    <ul className='flex flex-row right-0'>
+                        <li className={styles.signup}>
+                            <a href="/sign-up" className={styles.navlink2}>Sign Up</a>
+                        </li>
+                        <li className={styles.premium}>
+                            <a href="" >Premium</a>
+                        </li>
+                    </ul>
+                    </div>                    
+                </div>
+                <div className='mt-4 '>
+                    <ul className={`${styles.navmenu} ${isMenuActive ? styles.active : ''}`}>
+                        <li className={styles.navitem}>
+                            <a href="/live-games" className={styles.navlink}>Live Games</a>
+                        </li>
+                        <li className={styles.navitem}>
+                            <a href="/news" className={styles.navlink}>Latest News</a>
+                        </li>
+                        <li className={styles.navitem}>
+                            <a href="/season-stats" className={styles.navlink}>Season Stats</a>
+                        </li>
+                        <li className={styles.navitem}>
+                            <a href="/about" className={styles.navlink}>About</a>
+                        </li>
+                    </ul>
+                    <div className={`${styles.hamburger} ${isMenuActive ? styles.active : ''}`}>
+                        <span className={styles.bar}></span>
+                        <span className={styles.bar}></span>
+                        <span className={styles.bar}></span>
+                    </div>
                 </div>
             </div>
         </nav>
